bc3a1dd90f9a942b8908a28167ab07499bdcc7b0
John Pang <john@peathree.com>
2025-12-07 15:26:39 +0000

    Day 2 refactor

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,12 +1,13 @@
 import Day02.isInvalid1
 import Day02.isInvalid2
 import Day02.parse
+import Day02.solve
 
 fun main() {
 
-    fun part1(input: List<LongRange>): Long = input.flatMap { it.filter { it.isInvalid1() } }.sum()
+    fun part1(input: List<LongRange>): Long = input.solve { it.isInvalid1() }
 
-    fun part2(input: List<LongRange>): Long = input.flatMap { it.filter { it.isInvalid2() } }.sum()
+    fun part2(input: List<LongRange>): Long = input.solve { it.isInvalid2() }
 
     val input = parse(readInput("Day02").first())
     part1(input).println()
@@ -17,7 +18,7 @@ object Day02 {
 
     fun parse(s: String): List<LongRange> =
         s.split(",").map {
-            it.split("-").let { (first, second) -> first.toLong()..second.toLong() }
+            it.split("-").let { (id1, id2) -> id1.toLong()..id2.toLong() }
         }
 
     fun Long.isInvalid1(): Boolean = toString().let {
@@ -25,9 +26,11 @@ object Day02 {
         it.take(mid) == it.drop(mid)
     }
 
-    fun Long.isInvalid2(): Boolean = toString().let {
-        (1..it.length / 2).map { i ->
-            "^(${it.take(i)})*$".toRegex().matches(it)
+    fun Long.isInvalid2(): Boolean = toString().let { s ->
+        (1..s.length / 2).map { i ->
+            "^(${s.take(i)})*$".toRegex().matches(s)
         }.firstOrNull { it } ?: false
     }
+
+    fun List<LongRange>.solve(f: (Long) -> Boolean) = flatMap { it.filter { f(it) } }.sum()
 }
